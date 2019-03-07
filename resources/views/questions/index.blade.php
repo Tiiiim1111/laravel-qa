1e6e257273b69f0ffa1e34906edecf7e650abc28
Tiiiiiiiiim <Tim Tequillo>
2019-03-07 15:56:21 +0800

    Implement Delete Function and return the votes

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -20,12 +20,35 @@
                     @include ('layouts._messages')
                      @foreach ($questions as $question)
                     <div class="media">
+
+
+                            <div class="d-flex flex-column counters">
+                                    <div class="vote">
+                                    <strong>{{ $question->votes }}</strong>{{str_plural('vote',$question->votes)}}
+                                    </div>
+        
+                                    <div class="status {{$question->status}}">
+                                    <strong>{{ $question->answers }}</strong>{{str_plural('answers', $question->answers)}}
+                                    </div>
+        
+                                    
+                                    <div class="view">
+                                    {{ $question->views ." ".str_plural('view',$question->votes)}}
+                                    </div>
+                                </div>  
+
+
                         <div class="media-body">
                                 <div class="d-flex align-items-center">
                             <h3 class="mt-0"> <a href="{{ $question->url }}"> {{ $question->title }} </a> </h3>
                                     <div class="ml-auto">
                                     <a href="{{route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit </a>
-                                    </div>
+                                    <form class="form-delete" method="post" action="{{route('questions.destroy',$question->id)}}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">DELETE</button>
+                                    </form>
+                                </div>
                         </div>
                             <p class="lead">   
                                 Asked by
